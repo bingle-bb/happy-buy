765d5d96fed7b3dc3a58744ae698927605141417
revy <revy200311@gmail.com>
2025-08-15 14:13:48 +0630

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+import "@fontsource/montserrat";
+
+import App from "./App.jsx";
+import "./App.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
